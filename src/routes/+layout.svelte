af45875a2a07e9a576b78491ab85039a3c96fdcc
Steven Castro <steven.castro.fy@gmail.com>
2026-09-14 18:09:58 +0800

    fix: moved header info to layout component, updated page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,10 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Hornet's Calculator</title>
+	<meta name="description" content="Calculate Hornet's damage in Hollow Knight: Silksong using different tools, skills, crests, and effects."/>
+    <meta name="keywords" content="silksong calculator, hollow knight silksong, hornet damage calculator, silksong damage, hornet loadout, damage analysis"/>
+</svelte:head>
 {@render children()}
